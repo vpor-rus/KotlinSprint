095bf4366059a252361177ebb4f0e0e08d09bd08
Vasiliy Portko <vpor901@gmail.com>
2025-05-31 19:06:04 +1100

    KS-11-3 five abonent

--- src/main/kotlin/lesson_13/Task_3.kt
+++ src/main/kotlin/lesson_13/Task_3.kt
@@ -11,8 +11,6 @@ package org.example.lesson_13
 
 fun main() {
 
-     mutableListOf()
-
     val phoneDirectoriy1 = PhoneDirectoriy("Кот Леопольд", 89046758090, null,)
     val phoneDirectoriy2 = PhoneDirectoriy("Ситх", 12345654321, "Sitkh and Co")
     val phoneDirectoriy3 = PhoneDirectoriy("капитан Немо", 89000000000, null)
@@ -20,8 +18,10 @@ fun main() {
     val phoneDirectoriy5 = PhoneDirectoriy("Великий уравнитель", 3030, "null")
 
     val listPhoneDirectoriy: MutableList<PhoneDirectoriy> = mutableListOf(phoneDirectoriy1, phoneDirectoriy2,
-                                                        phoneDirectoriy3, phoneDirectoriy4, phoneDirectoriy4)
-    listPhoneDirectoriy.forEach {  }
+                                                        phoneDirectoriy3, phoneDirectoriy4, phoneDirectoriy5)
+
+    val listCompaniName = listPhoneDirectoriy.mapNotNull{ phoneDirectoriy -> phoneDirectoriy.companiName }
+    print(listCompaniName)
 }
 
 class PhoneDirectoriy(
