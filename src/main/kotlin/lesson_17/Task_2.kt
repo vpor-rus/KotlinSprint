d6afbaedcfb0c2f882df708e6e81b91ba501112f
Vasiliy Portko <vpor901@gmail.com>
2025-06-09 20:17:06 +1100

    KS-17-2 not new name

---
+++ src/main/kotlin/lesson_17/Task_2.kt
@@ -0,0 +1,20 @@
+package org.example.lesson_17
+
+/*
+Опиши класс для корабля в мобильной игре. Поля хранят имя, среднюю скорость и порт приписки.
+
+Имя корабля менять нельзя, при попытке перезаписать имя в консоль должно выводиться предупреждение.
+
+Создай экземпляр класса и протестируй работу.*/
+
+fun main() {
+val blackHugar = Ship("BlackHugar", 23, "Tartuga")
+    blackHugar.name = "NeverChange"
+}
+
+private class Ship(val name: String, var mediumSpeed: Int, var homePort: String,) {
+    var mutableName: String = name
+        set(value) {
+           field = print("Нельзя изменить имя корабля").toString()
+        }
+}
