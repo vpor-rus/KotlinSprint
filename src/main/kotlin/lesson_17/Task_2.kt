bc135b1a76695c55c2789104bf815e6510558a87
Vasiliy Portko <vpor901@gmail.com>
2025-06-09 20:19:48 +1100

    KS-17-2 not new name

--- src/main/kotlin/lesson_17/Task_2.kt
+++ src/main/kotlin/lesson_17/Task_2.kt
@@ -14,7 +14,8 @@ val blackHugar = Ship("BlackHugar", 23, "Tartuga")
 
 private class Ship(name: String, var mediumSpeed: Int, var homePort: String,) {
     var mutableName: String = name
+        get() = field
         set(value) {
-           field = print("Нельзя изменить имя корабля").toString()
+           field = print("Нельзя изменить имя корабля  ").toString()
         }
 }
