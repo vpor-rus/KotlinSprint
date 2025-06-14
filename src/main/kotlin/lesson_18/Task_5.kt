4a187d7e0d23ccf5c8de78029aa5f1eac9f1942d
Vasiliy Portko <vpor901@gmail.com>
2025-06-14 20:15:57 +1100

    KS-18-5 draw shape

--- src/main/kotlin/lesson_18/Task_5.kt
+++ src/main/kotlin/lesson_18/Task_5.kt
@@ -9,77 +9,68 @@ package org.example.lesson_18
 каких координатах. Используйте перегрузку методов для реализации.*/
 
 fun main() {
-
+    val shape1 = Screen()
+    shape1.draw(1, 2, 3, 4, 5, 6)
 }
 
-abstract class Screen{
-    abstract fun draw()
-}
-
-class CircleInt(
-    private val x: Int,
-    private val y: Int,
-    private val radius: Int,
-) : Screen() {
-    override fun draw() {
+class Screen {
+    fun draw(
+        x: Int,
+        y: Int,
+        radius: Int,
+    ) {
         println("Центр круга находится на координате $x и $y, вокруг центра рисуется окружность с радиусом $radius")
     }
-}
 
-class CircleFloat(
-    private  val x: Float,
-    private  val y: Float,
-    private val radius: Float,
-): Screen() {
-    override fun draw() {
+    fun draw(
+        x: Float,
+        y: Float,
+        radius: Float,
+    ) {
         println("Центр круга находится на координате $x и $y, вокруг центра рисуется окружность с радиусом $radius")
     }
-}
 
-class SquareInt(
-    private val x: Int,
-    private val y: Int,
-    private val top: Int,
-    private val left: Int,
-    private val right: Int,
-    private val bottom: Int,
-): Screen() {
-    override fun draw() {
-        println("Центр четырехугольника(точка пересечения диагоналей) располагается на координате $x и $y" +
-                "вокруг строится четырехугольник с левой стороной равной $left, верхней равной $top, " +
-                "правой равной $right, нижней равной $bottom")
+    fun draw(
+        x: Int,
+        y: Int,
+        top: Int,
+        left: Int,
+        right: Int,
+        bottom: Int,
+    ) {
+        println(
+            "Центр четырехугольника(точка пересечения диагоналей) располагается на координате $x и $y" +
+            " вокруг строится четырехугольник с левой стороной равной $left, верхней равной $top, " +
+            " правой равной $right, нижней равной $bottom"
+        )
     }
-}
 
-class SqureFloat(
-    private val x: Float,
-    private val y: Float,
-    private val top: Float,
-    private val left: Float,
-    private val right: Float,
-    private val bottom: Float,
-): Screen() {
-    override fun draw() {
-        "Центр четырехугольника(точка пересечения диагоналей) располагается на координате $x и $y" +
-                "вокруг строится четырехугольник с левой стороной равной $left, верхней равной $top, " +
-                "правой равной $right, нижней равной $bottom")
+    fun draw(
+        x: Float,
+        y: Float,
+        top: Float,
+        left: Float,
+        right: Float,
+        bottom: Float,
+    ) {
+        println(
+            "Центр четырехугольника(точка пересечения диагоналей) располагается на координате $x и $y" +
+                    " вокруг строится четырехугольник с левой стороной равной $left, верхней равной $top, " +
+                    " правой равной $right, нижней равной $bottom"
+        )
     }
-}
 
-class DotInt(
-    private  val x: Int,
-    private val y: Int,
-): Screen() {
-    override fun draw() {
-        println("на коорлинате $x и $y рисуется точка)
+    fun draw(
+        x: Int,
+        y: Int,
+    ) {
+        println("на коорлинате $x и $y рисуется точка")
     }
-}
 
-class DotInt(
-    private val x: Float,
-    private val y: Float,
-): Screen() {
-    override fun draw() {
-        println("на коорлинате $x и $y рисуется точка)
+    fun draw(
+        x: Float,
+        y: Float,
+    ) {
+        println("на коорлинате $x и $y рисуется точка")
     }
 }
