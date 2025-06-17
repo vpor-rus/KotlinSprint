25d6996d7bf69a46e8a5bb784f42325b358131a5
Vasiliy Portko <vpor901@gmail.com>
2025-06-17 19:19:41 +1100

    KS-19-2 product category

--- src/main/kotlin/lesson_19/Task_2.kt
+++ src/main/kotlin/lesson_19/Task_2.kt
@@ -1,8 +1,5 @@
 package org.example.lesson_19
 
-import org.example.lesson_10.PLAYER_VICTORY_RATE
-import org.jsoup.nodes.Range
-
 /*
 В интернет-магазине товары делятся на несколько категорий: одежда, канцелярские товары и разное.
 
@@ -13,26 +10,34 @@ import org.jsoup.nodes.Range
 – для вывода информации о товаре, для категории используй метод определения категории.*/
 
 fun main() {
-    val product1 = Product("чипсы", 1, ProductCategories.MISCELLANEOUS)
-    val product2 = Product("перьевая ручка", 2, ProductCategories.OFFICE_SUPPLIES)
-    val product3 = Product("ветровка", 3, ProductCategories.CLOTHING)
+    val product1 = Product("чипсы", 1, ProductCategory.MISCELLANEOUS)
+    val product2 = Product("перьевая ручка", 2, ProductCategory.OFFICE_SUPPLIES)
+    val product3 = Product("ветровка", 3, ProductCategory.CLOTHING)
+
     product1.printProperties()
     product2.printProperties()
     product3.printProperties()
 }
 
-enum class ProductCategories(val nameCategory: String){
-     CLOTHING("одежда"),
-     OFFICE_SUPPLIES("канцелярские товары"),
-     MISCELLANEOUS("разное"),
- }
+enum class ProductCategory() {
+     CLOTHING,
+     OFFICE_SUPPLIES,
+     MISCELLANEOUS;
+
+    fun nameToDispalay(): String =
+        when(this) {
+            CLOTHING -> "одежда"
+            OFFICE_SUPPLIES -> "канцелярские товары"
+            MISCELLANEOUS -> "разное"
+        }
+    }
 
 private class Product(
     val name: String,
     val id: Int,
-    val categories: ProductCategories,
+    val categories: ProductCategory,
 ) {
     fun printProperties() {
-        println("представляем вам $name находится в товарах категории ${categories.nameCategory} ячейке $id")
+        println("представляем вам $name находится в товарах категории ${categories.nameToDispalay()} ячейке $id")
     }
 }
